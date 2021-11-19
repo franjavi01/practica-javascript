fac69b0d703aa08b0d0bda9098fefc5920867719
franjavi01 <fjsg0001@gmail.com>
2021-11-19 20:04:34 +0100

    práctica javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+let saludo1 = document.getElementById("saludo1");
+saludo1.innerHTML = `Hola Mundo`;
+
+let saludo2 = document.getElementById("saludo2")
+saludo2.innerHTML = `Cogío el microfono y dijo: ` + saludo1.innerHTML
+
+let suma = document.getElementById("suma")
+suma.innerHTML = 7+5
+
+console.log(saludo1)
+console.log(saludo2)
+console.log(suma)
